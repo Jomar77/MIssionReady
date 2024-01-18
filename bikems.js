0bd3b8cbbd0c5337c474dd8bd1a7e6b3d447958d
JOMAR NACORDA <jnacorda04@gmail.com>
2024-01-18 21:42:50 +1300

    Refactor code to improve performance and readability

---
+++ bikems.js
@@ -0,0 +1,48 @@
+let bikePrice = [908, 1090, 563, 563, 5900]
+
+
+function mergeSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+
+    const mid = Math.floor(arr.length / 2);
+    const left = arr.slice(0, mid);
+    const right = arr.slice(mid);
+
+    return merge(mergeSort(left), mergeSort(right))
+}
+
+
+function merge(left, right) {
+    let result = [], leftIndex = 0, rightIndex = 0;
+
+
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            result.push(left[leftIndex]);
+            leftIndex++;
+        }
+        else {
+            result.push(right[rightIndex]);
+            rightIndex++;
+        }
+    }
+
+
+    while (leftIndex < left.length) {
+        result.push(left[leftIndex]);
+        leftIndex++;
+    }
+
+    while (rightIndex < right.length) {
+        result.push(right[rightIndex]);
+        rightIndex++;
+    }
+
+    return result;
+}
+
+
+
+console.log(mergeSort(bikePrice));
